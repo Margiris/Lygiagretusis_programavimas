d9d202487982d4f9d129e6ddd1826a4ce16fefa9
Margiris <margiris.burakauskas@gmail.com>
2018-10-29 09:21:27 +0200

    Make consumer class, further modify read method for reading orders' data. Finished methods for printing to file and console. Rename some variables for clarity.

--- L2a/IFF-6-10_BurakauskasM_L2a.cpp
+++ L2a/IFF-6-10_BurakauskasM_L2a.cpp
@@ -14,6 +14,8 @@ const string results_filename = R"(IFF-6-10_BurakauskasM_L2a_rez.txt)";
 const char delimiter = ',';
 const int total_size = 100;
 
+int data_count;
+
 // int producer_thread_count;
 // int consumer_thread_count;
 
@@ -25,18 +27,16 @@ struct car
     double price{};
 };
 
-struct item
+struct order
 {
     int year;
     int count;
 };
 
-vector<item> orders;
-int data_count;
 
 class monitor
 {
-    item available_cars_[total_size]{};
+    order available_cars_[total_size]{};
     int available_cars_count_;
     bool producers_exist_[total_size]{};
 
@@ -72,54 +72,101 @@ class monitor
 
 class producer
 {
-    vector<car> car_data_;
+    vector<car> cars_data_;
 
     public:
     explicit producer(const vector<car>& list_of_cars)
     {
-        car_data_ = list_of_cars;
+        cars_data_ = list_of_cars;
     }
 
     void add_car(const car& new_car)
     {
-        car_data_.push_back(new_car);
+        cars_data_.push_back(new_car);
     }
 
-    void print_car_data()
+    void print_cars_data()
     {
-        for (const auto& a_car : car_data_)
+        stringstream buffer;
+
+        buffer << left << setw(4) << "Nr." << setw(15) << "Gamintojas" << setw(20) << "Modelis" << right << setw(5) <<
+            "Metai" << setw(10) << fixed << setprecision(2) << "Kaina" << endl;
+        buffer << string(54, '-') << "\n";
+
+        auto line_index = 0;
+
+        for (const auto& a_car : cars_data_)
         {
-            cout << left << setw(15) << a_car.manufacturer << setw(20) << a_car.model << right << setw(5) << a_car.year
-                << setw(10) << fixed << setprecision(2) << a_car.price << "\n";
+            buffer << left << setw(4) << ++line_index << setw(15) << a_car.manufacturer << setw(20) << a_car.model <<
+                right << setw(5) << a_car.year << setw(10) << fixed << setprecision(2) << a_car.price << "\n";
         }
+
+        buffer << string(54, '-') << "\n";
+
+        ofstream results_file(results_filename, ios::app);
+        results_file << buffer.str();
+        cout << buffer.str();
     }
 
-    void produce()
+    // void produce()
+    // {
+    // }
+};
+
+class consumer
+{
+    vector<order> orders_data_;
+
+    public:
+    explicit consumer(const vector<order>& orders)
     {
+        orders_data_ = orders;
+    }
+
+    void add_order(const order& new_order)
+    {
+        orders_data_.push_back(new_order);
+    }
+
+    void print_orders_data()
+    {
+        stringstream buffer;
+
+        buffer << left << setw(4) << "Nr." << right << setw(6) << "Metai" << setw(7) << "Kiekis" << "\n";
+        buffer << string(17, '-') << "\n";
+
+        auto line_index = 0;
+
+        for (const auto& an_order : orders_data_)
+        {
+            buffer << left << setw(4) << ++line_index << right << setw(6) << an_order.year << setw(7) << an_order.count
+                << "\n";
+        }
+
+        buffer << string(17, '-') << "\n";
+
+        ofstream results_file(results_filename, ios::app);
+        results_file << buffer.str();
+        cout << buffer.str();
     }
 };
 
-struct consumer
+void read_producer_data(vector<producer>& producers, vector<consumer>& consumers)
 {
-};
-
-vector<producer> read_producer_data()
-{
-    vector<producer> producers;
-
-    ifstream file(data_filename);
+    ifstream data_file(data_filename);
 
     string current_line;
 
-    while (getline(file, current_line))
+    while (getline(data_file, current_line))
     {
-        const int producer_elements_count = stol(current_line);
+        const int elements_count = stol(current_line);
 
-        vector<car> car_data;
+        vector<car> cars_data;
+        vector<order> orders_data;
 
-        for (auto i = 0; i < producer_elements_count; i++)
+        for (auto i = 0; i < elements_count; i++)
         {
-            getline(file, current_line);
+            getline(data_file, current_line);
 
             vector<string> current_values;
             stringstream ss(current_line);
@@ -137,26 +184,48 @@ vector<producer> read_producer_data()
                 const int year = stol(current_values[2]);
                 const double price = stold(current_values[3]);
 
-                car_data.push_back(car{manufacturer, model, year, price});
+                cars_data.push_back(car{manufacturer, model, year, price});
             }
             else if (current_values.size() == 2)
             {
                 const int year = stol(current_values[0]);
                 const int count = stol(current_values[1]);
 
-                orders.push_back(item{year, count});
+                orders_data.push_back(order{year, count});
             }
         }
 
-        producers.emplace_back(car_data);
+        (cars_data.size() > orders_data.size()
+             ? producers.emplace_back(cars_data)
+             : consumers.emplace_back(orders_data));
     }
+}
 
-    return producers;
+void clear_results_file()
+{
+    ofstream file;
+    file.open(results_filename, ofstream::out | ofstream::trunc);
+    file.close();
 }
 
 int main()
 {
-    auto producers = read_producer_data();
+    clear_results_file();
+
+    vector<producer> producers;
+    vector<consumer> consumers;
+
+    read_producer_data(producers, consumers);
+
+    for (auto& producer : producers)
+    {
+        producer.print_cars_data();
+    }
+
+    for (auto& consumer : consumers)
+    {
+        consumer.print_orders_data();
+    }
 
     system("pause");
 }
