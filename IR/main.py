96aa69e664d66d3bb4779599c9cdcb1fc3170ce7
Margiris <34870816+Margiris@users.noreply.github.com>
2018-12-03 21:06:12 +0200

    Add main and gui files, update ignore to not version image files

---
+++ IR/main.py
@@ -0,0 +1,32 @@
+# Pillow >= 1.0 no longer supports “import Image”. Please use “from PIL import Image” instead.
+# Pillow >= 2.1.0 no longer supports “import _imaging”. Please use “from PIL.Image import core as _imaging” instead.
+
+from PIL import Image
+
+numberOfThreads = 12
+outputImagePath = "aaa.png"
+
+# inputImagePath = "test1.png"
+inputImagePath = "to2.jpg"
+
+
+def brightness(point):
+    return point * 10
+
+
+image = Image.open(inputImagePath, 'r')
+
+pixel_values = list(image.getdata())
+
+# for r, g, b, a in pixel_values:
+#     print(a)
+
+# print(image.point())
+
+out = image.point(lambda i: brightness(i))
+
+# out.show()
+
+out.save(outputImagePath)
+
+print(image.format, image.size, image.mode)
